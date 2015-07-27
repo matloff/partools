53e456671b8a1ad396021e5f38cf99f4e6492376
Norm Matloff <matloff@cs.ucdavis.edu>
2015-07-26 23:29:56 -0700

    update

--- R/Snowdoop.R
+++ R/Snowdoop.R
@@ -63,17 +63,17 @@ mysortedchunk <- function(mybds,basenm,ndigs,colnum,outname,
    myhi <- mybds[2]
    if (usefread) {
       require(data.table)
-      ### myfread <- fread
-   } ### else myfread <- read.table
+      myfread <- fread
+   } else myfread <- read.table
    for (i in 1:ncls) {
-      if (usefread) {
-         tmp <- as.data.frame(
-            fread(filechunkname(basenm,ndigs,i),header=header,sep=sep) 
-            )
-      } else
-         tmp <- 
-            read.table(filechunkname(basenm,ndigs,i),header=header,sep=sep)
-      ### tmp <- myfread(filechunkname(basenm,ndigs,i),header=header,sep=sep) 
+##       if (usefread) {
+##          tmp <- as.data.frame(
+##             fread(filechunkname(basenm,ndigs,i),header=header,sep=sep) 
+##             )
+##       } else
+##          tmp <- 
+##             read.table(filechunkname(basenm,ndigs,i),header=header,sep=sep)
+      tmp <- myfread(filechunkname(basenm,ndigs,i),header=header,sep=sep) 
       tmpcol <- tmp[,colnum]
       if (me == 1) {
          tmp <- tmp[tmpcol <= myhi,] 
