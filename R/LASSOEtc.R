9cde6ab393e2f1720f1308a7a44cedc505e51849
Norm Matloff <matloff@cs.ucdavis.edu>
2016-07-25 23:31:24 -0700

    just starting

---
+++ R/LASSOEtc.R
@@ -0,0 +1,23 @@
+
+# calars(), SA computation of lars()
+
+# arguments:
+
+#    cls: 'parallel' cluster
+#    ddf: name of a distributed data frame
+#    xcols: column numbers of ddf of X matrix
+#    ycol: column number of ddf of Y vector
+#    outfl: name of output file
+
+calars <- function(cls,ddf,xcols,ycol) {
+
+}
+
+
+##  data(prgeng) 
+##  pe <- prgeng 
+##  x <- pe[,c(1,6,8)] 
+##  y <- pe$wageinc 
+##  xx <- cbind(x,matrix(rnorm(nrow(pe)*500),ncol=500)) 
+##  system.time(o1 <- lars(xx,y)) 
+
