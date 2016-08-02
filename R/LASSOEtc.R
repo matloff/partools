3fc859439fe746f17a4d37e822305f3b28062dc0
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2016-08-02 16:02:45 -0700

    still on LA*R

--- R/LASSOEtc.R
+++ R/LASSOEtc.R
@@ -4,17 +4,20 @@
 # arguments:
 
 #    cls: 'parallel' cluster
-#    ddf: name of a distributed data frame
-#    xcols: column numbers of ddf of X matrix
+#    ddf: quoted name of a distributed data frame
+#    xcols: quoted column numbers of ddf of X matrix
 #    ycol: column number of ddf of Y vector
 #    type: type of analysis
 #    outfl: name of output file
 
-calars <- function(cls,ddf,xcols,ycol,type='lasso',outfl=NULL) {
-   clusterExport(cls,c('xcols','ycol'),envir=environment())
-   cmd <- paste('x <<- as.matrix(',ddf,'[,',xcols,'])',sep='')
-   doremotecmd(cls,cmd)
-   cmd <- paste('y <<- as.matrix(',ddf,'[,',ycol,'])',sep='')
+# calars <- function(cls,ddf,xcols,ycol,type='lasso',outfl=NULL) {
+calars <- function(cls,larscmd,outfl=NULL) {
+#    clusterExport(cls,c('xcols','ycol'),envir=environment())
+#    cmd <- paste('x <- as.matrix(',ddf,'[,',xcols,'])',sep='')
+#    doremotecmd(cls,cmd)
+#    cmd <- paste('y <- as.matrix(',ddf,'[,',ycol,'])',sep='')
+#    doremotecmd(cls,cmd)
+   cmd <- paste('larsout <- ',larscmd,sep='')
    doremotecmd(cls,cmd)
 }
 
@@ -25,11 +28,13 @@ doremotecmd <- function(cls,cmd) {
    clusterEvalQ(cls,docmd(cmd))
 }
 
-
-##  data(prgeng) 
-##  pe <- prgeng 
-distribsplit(cls,'pe') 
-clusterEvalQ(cls,head(pe)) 
-calars(cls,'pe',c(1,7),8) 
-clusterEvalQ(cls,head(x)) 
-
+# library(partools)
+# cls <- makeCluster(2)
+# setclsinfo(cls)
+# data(prgeng) 
+# pe <- prgeng 
+# distribsplit(cls,'pe') 
+# clusterEvalQ(library(lars))
+# calars(cls,'lars(as.matrix(pe[,c(7,8)]),pe[,8])')
+# clusterEvalQ(cls,head(x)) 
+# 
