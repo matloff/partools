be8c9588d4895c0a26c281e666a16dc95747955e
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-04-17 12:22:06 -0700

    failing test for aggregate

--- tests/testthat/testutils.R
+++ tests/testthat/testutils.R
@@ -4,6 +4,7 @@ cls <- makeCluster(2)
 setclsinfo(cls)
 
 az <- data.frame(letter = letters, number = seq_along(letters))
+azaz <- rbind(az, az)
 
 test_that("formrowchunks exports data", {
 
@@ -12,5 +13,20 @@ test_that("formrowchunks exports data", {
     az2 <- do.call(rbind, chunks)
     az2 <- az2[order(az2$number), ]
     expect_equal(az, az2)
+
+})
+
+
+test_that("distrib* functions", {
+
+    distribsplit(cls, "azaz")
+    azback <- distribcat(cls, "azaz")
+    expect_equal(azaz, azback)
+
+    az2 <- distribagg(cls, ynames = "number", xnames = "letter"
+                      , dataname = "azaz", FUN = "mean")
+
+    expect_equal(az, az2)
+
 
 })
