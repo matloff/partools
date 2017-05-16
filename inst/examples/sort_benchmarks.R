696b82ab07f4822d30267d6afe2d434f5f5b55c7
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-05-16 11:16:11 -0700

    now I need to be able to pass more arguments into read.table

--- inst/examples/sort_benchmarks.R
+++ inst/examples/sort_benchmarks.R
@@ -13,5 +13,9 @@ unsorted[, 2] <- as.integer(paste0("0x", unsorted[, 2]))
 # Regular R takes about 115 ms to do 1 million records
 sorted <- unsorted[order(unsorted[, 1]), ]
 
-
 distribsplit(cls, "unsorted")
+
+filesave(cls, "unsorted", newbasename = "unsorted", ndigs = 2, sep = ",")
+
+filesort(cls, infilenm = "unsorted", colnum = 1, outdfnm = "unsorted"
+         , infiledst = TRUE, ndigs = 2, sep = ",")
