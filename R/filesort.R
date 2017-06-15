4a34248e4811f7f4faaaf096f3704446823e5aa2
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-06-15 14:48:20 -0700

    first test for disk sorting

--- R/filesort.R
+++ R/filesort.R
@@ -1,20 +1,17 @@
-# Generate corresponding .Rd file
-# roxygen2::rd_roclet
-
-
 #' Sort File On Disk
 #'
-#' Sorts 
+#' This function was designed to handle files larger than memory. At most
+#' \code{nrows} will be present in memory at once.
 #'
 #' @param infile unsorted file to read from. See \code{\link[utils]{read.table}}.
 #' @param outfile where to write the sorted file. See
-#' \code{\link[utils]{write.table}}. Default prepends "sorted_" to
-#' \code{infile}
+#' \code{\link[utils]{write.table}}. If \code{infile} is the name of a file
+#' then the default prepends "sorted_" to this name.
 #' @param sortcolumn which column of the data frame to sort on
 #' @param nrows number of rows in the data.frame held in memory
 #' @param read.table.args named list of extra arguments to read.table
 #' @param write.table.args named list of extra arguments to write.table.
-#' Defaults to using read.table args
+#' Defaults to using read.table.args to preserve the original formatting.
 #' @export
 disksort = function(infile
                     , outfile = NULL
---
+++ tests/testthat/test_disksort.R
@@ -0,0 +1,30 @@
+library(partools)
+
+context("disksort")
+
+fname_unsorted = "4132.txt"
+lines_unsorted = "4 four
+1 one
+3 three
+2 two"
+
+lines_sorted = "1 one
+2 two
+3 three
+4 four"
+
+writeChar(lines_unsorted, fname_unsorted)
+
+
+test_that("all defaults", {
+
+    disksort(fname_unsorted)
+    fname_sorted = paste0("sorted_", fname_unsorted)
+    actual = readChar(fname_sorted, nchars = 1000L)
+    expect_equal(lines_sorted, actual)
+    unlink(fname_sorted)
+
+})
+
+
+unlink(fname_unsorted)
