0e86f084cbb7de98a2a899ea74e7a892971211be
matloff <matloff@cs.ucdavis.edu>
2017-07-17 21:17:57 -0700

    doc for ddf

---
+++ man/ddf.R
@@ -0,0 +1,27 @@
+\name{findrow,makedff,'['}
+\alias{findrow}
+\alias{makedff}
+\alias{'['}
+
+\title{Virtual Data Structures}
+
+\description{
+Accessing a Distributed Data Frame or Similar Object As a Virtual
+Monolithic Object}
+
+\usage{
+findrow(cls, i, objname)
+makeddf(dname,cls) 
+}
+
+\arguments{
+  \item{cls}{A cluster run under the \pkg{parallel} package.}
+  \item{i}{A row number in a distributed data frame or similar object.}} 
+  \item{objname}{Name of such an object.}
+  \item{dname}{Name of such an object.}
+}
+
+\author{
+Norm Matloff and Reed Davis
+}
+
