6a0e48cd032b1d81b5170ff82ec752850912a871
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2017-11-26 14:22:04 -0800

    new quick-and-dirty debugging tools

--- R/DebugSnow.R
+++ R/DebugSnow.R
@@ -1,8 +1,80 @@
 
-# (in addition to dbs(), see "quick and dirty" utils at the end of this
-# file)
+# These debugging tools for partools fall into two groups:
 
-# dbs() ("debug Snow") and the other functions here serve to automate
+# 1.  dbq:  "Quick and dirty" tools, meaning just saving state to disk, as
+# with use of print statements in most languages.
+ 
+# 2.  dbs:  "Debug screen":  Makes use of R's internal debugging facility
+# to be able to watch program execution on the screen.
+
+############ dbq:  "quick and dirty" debugging tools ################
+
+# "quick and dirty" debugging, by printing/saving to files
+
+# dbqmsg():  prints messages to file; node 1 prints messages to the file
+# dbq.001, node 2 to dbq.002 etc., for later viewing by dbqview(); must
+# initialize with dbqmsgstart()
+
+# dbqsave(), dbqload(): save object to a file (only one per run), for
+# later reloading
+
+# dbqdump(): call to R's dump.frames()
+
+# initialize
+dbqmsgstart <- function(cls) {
+   ncls <- length(cls)
+   ndigs <- getnumdigs(ncls)
+   for (i in 1:length(cls))
+      cat("\n",file=filechunkname("dbq",ndigs,i),"\n")
+}
+
+# worker writes the given message to the worker's file
+dbqmsg <- function(msg) {
+   pte <- getpte()
+   ndigs <- getnumdigs(pte$ncls)
+   fn <- filechunkname("dbq",ndigs)
+   cat(msg,file=fn,append=TRUE,"\n")
+}
+
+# view messages save by dbqmsg() for this worker
+dbqview <- function(cls,wrkrNum) {
+   ndigs <- getnumdigs(length(cls))
+   n0s <- ndigs - nchar(as.character(wrkrNum))
+   fname <- paste('dbq.',wrkrNum,sep='')
+   writeLines(readLines(fname))
+}
+
+# save obj to disk, with suffix indicating the worker number
+dbqsave <- function(obj) {
+   pte <- getpte()
+   ndigs <- getnumdigs(pte$ncls)
+   fn <- filechunkname("dbqo",ndigs)
+   save(obj,file=fn)
+}
+
+# load saved object from disk, with suffix indicating worker number
+# wrkrNum
+dbqload <- function(cls,wrkrNum) {
+   ndigs <- getnumdigs(length(cls))
+   n0s <- ndigs - nchar(as.character(wrkrNum))
+   fname <- paste('dbqo.',wrkrNum,sep='')
+   load(fname)
+   obj
+}
+
+dbqdump <- function() {
+   pte <- getpte()
+   ndigs <- getnumdigs(pte$ncls)
+   fn <- filechunkname("dbqlastdump",ndigs)
+   cmd <- paste('dump.frames("',fn,'",to.file=TRUE)',sep='')
+   eval(parse(text=cmd))
+}
+
+#################### dbs debugging facility ######################
+
+# Requires Unix (Mac, Linux or Cygwin), for the 'screen' facility.
+
+# dbs() ("debug screen") and the other functions here serve to automate
 # the process of debugging code written in Snow, meaning the portion of
 # R's "parallel" library derived from the old "snow" package, e.g. the
 # function clusterApply(); this part of the library will be referred to
@@ -10,8 +82,6 @@
 
 # may also be used to debug Rdsm applications, since that library uses
 # snow for launching
-
-# requires the Unix utility "screen" (Linux, Mac, Cygwin)
 
 # typical usage pattern (see alsodbscat() below):
 
@@ -205,31 +275,3 @@ killdebug<- function() {
 #      as in usual Snow; NOTE:  however, while in browser mode, input 
 #       will be directly from the user typing into the worker window (or
 #       from writewrkrscreens() from the original window)
-
-###################################################################
-
-# "quick and dirty" debugging, by printing to a file; node 1 prints
-# messages to the file dbs.001, node 2 to dbs.002 etc.
-
-# initialize
-dbsmsgstart <- function(cls) {
-   ncls <- length(cls)
-   ndigs <- getnumdigs(ncls)
-   for (i in 1:length(cls))
-      cat("\n",file=filechunkname("dbs",ndigs,i),"\n")
-}
-
-dbsmsg <- function(msg) {
-   pte <- getpte()
-   ndigs <- getnumdigs(pte$ncls)
-   fn <- filechunkname("dbs",ndigs)
-   cat(msg,file=fn,append=TRUE,"\n")
-}
-
-dbsdump <- function() {
-   pte <- getpte()
-   ndigs <- getnumdigs(pte$ncls)
-   fn <- filechunkname("last.dump",ndigs)
-   cmd <- paste('dump.frames("',fn,'",to.file=TRUE)',sep='')
-   eval(parse(text=cmd))
-}
