93d69fb381eac573ca0fc47a065948e568e74794
Norm Matloff <matloff@cs.ucdavis.edu>
2018-05-22 00:02:57 -0700

    still working

--- R/HQS.R
+++ R/HQS.R
@@ -10,8 +10,8 @@
 #
 #    cls: 'parallel' cluster
 #    xname: data frame argument; if NULL, there is already a
-#           distributed data frame with name 'xname' on the 
-#           cluster; otherwise one will be created
+#           1-column distributed data frame with name 'xname' on 
+#           the cluster; otherwise one will be created
 # 
 # value:
 #
@@ -26,68 +26,59 @@ hqs <- function(cls,xname=NULL){
 
   # this function, to be executed by each worker node, does the main
   # work
-  hqsWorker <-function(){
-    # myID <- partoolsenv$myid
-    # groupSize <- partoolsenv$ncls
-    chunk <- as.vector(t(xname))
+  hqsWorker <-function() {
+    myID <- partoolsenv$myid
+    groupSize <- partoolsenv$ncls
+    chunk <- as.vector(xname[,1])
     #ptm <- proc.time()
     
     while (groupSize > 1){
-      
+
+      # this node's ID with respect to current subcube
       myrank <- (myID %% groupSize)
-      
-      if (myrank==0){
+      if (myrank == 0){
         pivot <- median(chunk)
         for (i in 1:(groupSize-1)){
           ptMEsend(pivot,myID-i)}
-        
       }
-      
       else{
-        
         pivot<-ptMErecv(myID+(groupSize-myrank))
-        
       }
-      
       lower<- chunk[chunk < pivot]
       upper<- chunk[chunk >= pivot]
-      
       if (myrank <= (groupSize/2) && myrank > 0) {
-        
         ptMEsend(upper,myID+(groupSize/2) )
-        
         newUpper<-ptMErecv(myID+(groupSize/2))
-        
         chunk<-c(lower, newUpper)
-        
       }
       else {
-        
         newLower<-ptMErecv(myID-(groupSize/2))
-        
         ptMEsend(lower,myID-(groupSize/2))
-        
         chunk<-c(newLower,upper)
-        
       }
       groupSize<-groupSize/2
-      
     }
     
-    chunk<-sort(chunk)
+    chunk <- sort(chunk)
     chunk
     #time<-proc.time() - ptm
     #time
     
   }
+
   clusterCall(cls, hqsWorker)
+
 }
 
 
 # enter host pcs (a power of 2 nodes) name as list of strings, 
 # with pc names,IP addresses, or run on "localhost"
-hostpcs<-c(rep("localhost",4))
-cls<-makeCluster(hostpcs)
-# enter test data
-data<-data.frame(sample(1:50, 1000, replace = TRUE))
-hqs(cls,data)
+testhqs <- function() 
+{
+   hostpcs <- c(rep("localhost",4))
+   cls <- makeCluster(hostpcs)
+   # enter test data
+   data <- data.frame(sample(1:50, 1000, replace = TRUE))
+   hqs(cls,data)
+}
+
